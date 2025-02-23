b2c21feedfece140811a2dbdb0e1ecb91304dc05
Manuel Valdés <manuelvaldesg97@gmail.com>
2025-02-23 16:00:57 -0300

    Creación de proyecto java spring boot, con integración a RabbitMQ. Microservicio productor.

---
+++ src/main/java/com/microservicio/registros_historicos_productor/RegistrosHistoricosProductorApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio.registros_historicos_productor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistrosHistoricosProductorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistrosHistoricosProductorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/microservicio/registros_historicos_productor/RegistrosHistoricosProductorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio.registros_historicos_productor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistrosHistoricosProductorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
